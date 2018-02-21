b150e4a8f90ffabd9909e691fc0f88f35a99a6e6
Zelimir Stefanovic <zelimirs@ccbill.com>
2018-02-21 09:04:31 +0100

    Formatting

--- cinema/src/main/webapp/resources/app/common/app.js
+++ cinema/src/main/webapp/resources/app/common/app.js
@@ -6,22 +6,18 @@ var cinemaApp = angular.module('cinemaApp', [ 'ngRoute', 'cinemaHomeModule', 'ci
 cinemaApp.config([ '$routeProvider', function($routeProvider, $compile) {
 
 	// Routing for application - routes the application to different pages without reloading the entire application.
-	$routeProvider
-	.when('/', {
+	$routeProvider.when('/', {
 		templateUrl : 'resources/app/common/home.html',
-		controller  : 'homeController'
-    })
-    .when('/movies', {
+		controller : 'homeController'
+	}).when('/movies', {
 		templateUrl : 'resources/app/movie/movies.html',
-		controller  : 'movieController'
-    })
-    .when('/movies/edit/:id', {
+		controller : 'movieController'
+	}).when('/movies/edit/:id', {
 		templateUrl : 'resources/app/movie/add-edit.html',
-		controller  : 'movieController'
-    })
-    .when('/movies/add', {
+		controller : 'movieController'
+	}).when('/movies/add', {
 		templateUrl : 'resources/app/movie/add-edit.html',
-		controller  : 'movieController'
-    })
-    
+		controller : 'movieController'
+	})
+
 } ])
